f3cdd0f4abf9cbfe331696efea71b68e59442de9
Jeff Muizelaar <jrmuizel@gmail.com>
2024-04-01 16:08:42 -0400

    Change default port to 80
    Without this netlify doesn't return data for connections to 4200

--- ember-cli-live-reload.js
+++ ember-cli-live-reload.js
@@ -3,9 +3,9 @@
   var host = location.hostname || 'localhost';
   var useCustomPort = false || location.port !== 4200;
   var defaultPort = location.port || (location.protocol === 'https:' ? 443 : 80);
-  var port = useCustomPort ? 4200 : defaultPort;
+  var port = useCustomPort ? 80 : defaultPort;
   var path = '&path=_lr/livereload';
-  var prefixURL = useCustomPort ? (location.protocol || 'http:') + '//' + host + ':' + 4200 : '';
+  var prefixURL = useCustomPort ? (location.protocol || 'http:') + '//' + host + ':' + 80 : '';
   var src = srcUrl || prefixURL + '/_lr/livereload.js?port=' + port + '&host=' + host + path;
   var script    = document.createElement('script');
   script.type   = 'text/javascript';
